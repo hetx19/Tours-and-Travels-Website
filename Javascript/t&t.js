3d974d098c4d88bd361db8e524cd7dc964494292
Het <hetsavaliya19@gmail.com>
2022-02-06 18:42:48 +0530

    Initial work Day 1

---
+++ Javascript/t&t.js
@@ -0,0 +1,9 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.form');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelector('.vid-btn');
+
